6a668f38b00f7f9c3e9475d0826de0609b3ff8f9
KataytsevYuriy <yuriy.kataytsev@gmail.com>
2022-05-22 22:14:36 +0300

    addHomework3

---
+++ 3/Vector/Vector/Pair.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    internal class Pair
+    {
+        public int Number { get; set; }
+        public int Freq { get; set; }
+        public Pair(int number, int freq)
+        {
+            Number = number;
+            Freq = freq;
+        }
+        public override string ToString()
+        {
+            return $"{Number} - {Freq}";
+        }
+    }
+}
---
+++ 3/Vector/Vector/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Vector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector arr = new Vector(20);
+            arr.InitRandom(1, 5);
+            Console.WriteLine(arr);
+            Pair[] pairs = arr.CalculateFreq();
+            for(int i = 0; i < pairs.Length; i++)
+            {
+                Console.WriteLine(pairs[i]);
+            }
+            arr.InitPoliandr(1, 5);
+            Console.WriteLine(arr);
+            Console.WriteLine("Is Poliandr {0}", arr.IsPoliand());
+            arr.InitRandom(1,5);
+            Console.WriteLine(arr);
+            Console.WriteLine("Is Poliandr {0}", arr.IsPoliand());
+
+            Console.WriteLine("Matrix");
+            Matrix matrix = new Matrix(7, 7);
+            matrix.DiagonalSnake(Start.right);
+            Console.WriteLine(matrix);
+            Console.WriteLine("New array");
+            Matrix matrix1 = new Matrix(7, 4);
+            matrix1.DiagonalSnake(Start.down);
+            Console.WriteLine(matrix1);
+        }
+    }
+}
