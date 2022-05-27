ef62b4dbe8ed5e5fa313b73341a2cbf3219ddf16
KataytsevYuriy <yuriy.kataytsev@gmail.com>
2022-05-27 17:59:49 +0300

    addTest5

---
+++ Test_5/Vector/Pair.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    internal class Pair
+    {
+        public int Number { get; set; }
+        public int Freq { get; set; }
+        public Pair(int number, int freq)
+        {
+            Number = number;
+            Freq = freq;
+        }
+        public override string ToString()
+        {
+            return $"{Number} - {Freq}";
+        }
+        public override bool Equals(object obj)
+        {
+            Pair pair = obj as Pair;
+            if (pair == null) return false;
+            return pair.Number == Number && pair.Freq == Freq; 
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ Test_5/Vector/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Vector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector arrBubble;                   //Clone aray for different algoritms
+            Vector arrQS;
+            Vector arrSM;
+            Vector arrPyr;
+            string fileName = "randomArray";
+            if (FileAccess.ReadArrayFromFile(fileName, out int[] fileArray))
+            {
+                Console.WriteLine("Reading array from file");
+                arrBubble = new Vector(fileArray);
+                arrQS = new Vector(fileArray);
+                arrSM = new Vector(fileArray);
+                arrPyr = new Vector(fileArray);
+            }
+            else
+            {
+                int n = 10000;
+                arrBubble = new Vector(n);
+                arrBubble.InitRandom(1, 100);
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    arr[i] = arrBubble[i];
+                }
+                arrQS = new Vector(arr);
+                arrSM = new Vector(arr);
+                arrPyr = new Vector(arr);
+                FileAccess.WriteArrayToFile(fileName, arr);
+            }
+            Console.WriteLine("Unsorted array");
+            Console.WriteLine(arrBubble);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            arrBubble.Bubble();
+            stopwatch.Stop();
+            TimeSpan bubble = stopwatch.Elapsed;
+            Console.WriteLine("Sorted array");
+            Console.WriteLine(arrBubble);
+            stopwatch.Restart();
+            arrQS.QuickSort();
+            stopwatch.Stop();
+            TimeSpan qs = stopwatch.Elapsed;
+            stopwatch.Restart();
+            arrSM.SplitMergeSort();
+            stopwatch.Stop();
+            TimeSpan sM = stopwatch.Elapsed;
+            stopwatch.Restart();
+            arrPyr.PyramidalSort();
+            stopwatch.Stop();
+            TimeSpan pyr = stopwatch.Elapsed;
+            Console.WriteLine("bubble = {0} sec and {1}msec", bubble.Seconds, bubble.Milliseconds);
+            Console.WriteLine("qSort = {0} sec and {1}msec", qs.Seconds, qs.Milliseconds);
+            Console.WriteLine("Split and Merge = {0} sec and {1}msec", sM.Seconds, sM.Milliseconds);
+            Console.WriteLine("Pyramidal = {0} sec and {1}msec", pyr.Seconds, pyr.Milliseconds);
+                            //Some result
+                            //bubble = 1 sec and 553msec
+                            //qSort = 0 sec and 8msec
+                            //Split and Merge = 0 sec and 5msec
+                            //Pyramidal = 0 sec and 9msec
+        }
+    }
+}
