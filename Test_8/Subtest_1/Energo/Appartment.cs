da51e8183c3af3302885c908bc704d7f77f605a2
Юрій Катайцев <kataytsev-ys@utg.ua>
2022-06-13 20:57:16 +0300

    addTest8-1

---
+++ Test_8/Subtest_1/Energo/Appartment.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Energo
+{
+    internal class Appartment
+    {
+        public int Number { get; set; }
+        public string FIO { get; set; }
+        public int startCounter { get; set; }
+        public int endCounter { get; set; }
+        public List<DateTime> counterReadingDates { get; set; }
+        public Appartment()
+        {
+            FIO = string.Empty;
+            counterReadingDates = new List<DateTime>();
+        }
+        public Appartment(string[] data)
+        {
+            if (data.Length < 4) throw new ArgumentException("Not enaph arguments");
+            counterReadingDates = new List<DateTime>();
+            string errors = string.Empty;
+            if (int.TryParse(data[0], out int number))
+            {
+                Number = number;
+            }
+            else
+            {
+                errors += "error in appartment number; ";
+            }
+            if (data[1].Length > 3)
+            {
+                FIO = data[1];
+            }
+            else
+            {
+                errors += "error in appartment owner; ";
+            }
+            if (int.TryParse(data[2], out int startCount))
+            {
+                startCounter = startCount;
+            }
+            else
+            {
+                errors += "error in appartment start counter; ";
+            }
+            if (int.TryParse(data[3], out int endCount))
+            {
+                endCounter = endCount;
+            }
+            else
+            {
+                errors += "error in appartment start counter; ";
+            }
+            if (data.Length > 4)
+            {
+                for (int i = 4; i < data.Length; i++)
+                {
+                    if (DateTime.TryParse(data[i], out DateTime date))
+                    {
+                        counterReadingDates.Add(date);
+                    }
+                    else
+                    {
+                        errors += $"error in appartment counter reading date number {i - 3}; ";
+                    }
+                }
+                counterReadingDates=counterReadingDates.OrderBy(d => d.Date).ToList();
+            }
+            if(!string.IsNullOrEmpty(errors)) throw new FormatException(errors);
+        }
+        public override string ToString()
+        {
+            string dates = "";
+            foreach (DateTime date in counterReadingDates)
+            {
+                dates += $"{date.ToLongDateString()}\t";
+            }
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}",
+                Number, FIO, startCounter, endCounter, dates);
+        }
+    }
+}
---
+++ Test_8/Subtest_1/Energo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Energo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppartmensService appartmensService = new AppartmensService(@"..\..\..\data.txt");
+            AppartmensService appartmensService1 = new AppartmensService(@"..\..\..\data1.txt");
+            Console.WriteLine("  a + b");
+            Console.WriteLine(appartmensService + appartmensService1);
+            Console.WriteLine("  a - b");
+            Console.WriteLine(appartmensService1-appartmensService);
+        }
+    }
+}
