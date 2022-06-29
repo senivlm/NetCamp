76f369b4dd7c299a2c543a8ea9cb194304cb0826
KataytsevYuriy <yuriy.kataytsev@gmail.com>
2022-06-29 16:20:05 +0300

    addTest12-1

---
+++ Test_12/Subtest_1/ProductsApp/Services/CreateProductFromConsoleService.cs
@@ -0,0 +1,74 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsApp.Services
+{
+    static internal class CreateProductFromConsole
+    {
+        public static Product CreateProduct()
+        {
+            bool seccessful = true;
+            Product product = null;
+            Console.WriteLine("Створення продукту");
+            do
+            {
+                Console.WriteLine("Введіть назву");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введіть вагу");
+                string weightStr = Console.ReadLine();
+                Console.WriteLine("Введіть цiну");
+                string priceStr = Console.ReadLine();
+                try
+                {
+                    product = new Product(name, priceStr, weightStr);
+                    seccessful = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Продукт не створено. Повторіть ввод.");
+                    seccessful = false;
+                }
+            } while (!seccessful);
+            return product;
+        }
+        public static Meat CreateMeat()
+        {
+            Meat meat = null;
+            bool seccessful = true;
+            Console.WriteLine("Створення м'ясного продукту");
+            do
+            {
+                Console.WriteLine("Введіть назву");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введіть вагу");
+                string weightStr = Console.ReadLine();
+                Console.WriteLine("Введіть цiну");
+                string priceStr = Console.ReadLine();
+                Console.WriteLine("Введіть строк вжитку");
+                string useBeforStr = Console.ReadLine();
+                Console.WriteLine("Введіть категорію");
+                string categoryStr = Console.ReadLine();
+                Console.WriteLine("Введіть вид");
+                string kindStr = Console.ReadLine();
+                try
+                {
+                    if (!(Double.TryParse(priceStr, out double price) && Double.TryParse(weightStr, out double weight))) throw new ArgumentException();
+                    meat = new Meat(name, price, weight, useBeforStr, categoryStr, kindStr);
+                    seccessful = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("М'ясо не створено. Повторіть ввод.");
+                    seccessful = false;
+                }
+            } while (!seccessful);
+            return meat;
+        }
+    }
+}
---
+++ Test_12/Subtest_1/ProductsApp/Services/FileService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsApp.Services
+{
+    static internal class FileService
+    {
+        public static void AddStringToFile(string fileName, string data, bool appendData = true)
+        {
+            using (StreamWriter stream = new (fileName, appendData))
+            {
+                stream.WriteLine(data);
+            }
+        }
+        public static List<string> GetStringsFromFile(string fileName)
+        {
+            List<string> res = new ();
+            using (StreamReader stream = new (fileName))
+            {
+                while (!stream.EndOfStream)
+                {
+                    res.Add(stream.ReadLine());
+                }
+            }
+            return res;
+        }
+    }
+}
